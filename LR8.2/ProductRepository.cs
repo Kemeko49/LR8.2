cdadc8f1f9fd21a96063416d749fc48c183ffa49
Kemeko <artemovcharenko30@gmail.com>
2024-03-25 21:41:14 +0200

    Добавьте файлы проекта.

---
+++ LR8.2/ProductRepository.cs
@@ -0,0 +1,22 @@
+﻿public static class ProductRepository
+{
+    static List<Product> Products = new List<Product>();
+
+    public static IList<Product> GetProductList() => Products;
+
+    public static bool AddProduct(Product? product)
+    {
+        if (product == null)
+        {
+            return false;
+        }
+        Products.Add(product);
+        return true;
+    }
+
+    public static void init()
+    {
+        Products = new List<Product> {
+        };
+    }
+}
